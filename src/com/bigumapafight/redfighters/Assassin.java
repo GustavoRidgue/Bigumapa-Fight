0ed912346b6f72d85932a1e22977a01138221603
Gustavo Ridgue <guferrari007@gmail.com>
2020-11-13 23:06:50 -0300

    Rewrite dragon's special attack and do assassin's special attack

--- src/com/bigumapafight/redfighters/Assassin.java
+++ src/com/bigumapafight/redfighters/Assassin.java
@@ -1,5 +1,7 @@
 package com.bigumapafight.redfighters;
 
+import com.bigumapafight.progenitor.Fighter;
+
 public class Assassin extends RedFighter {
     public Assassin(String name) {
         super(name);
@@ -23,5 +25,49 @@ public class Assassin extends RedFighter {
 
         this.setLegendaryAttack(900);
         this.setLegendaryAttackElixir(900);
+    }
+
+    public void specialMegaAttack(Fighter targetFighter) {
+        if (this.verifyCombat() && targetFighter.verifyCombat()) {
+            this.setElixir(this.getElixir() - (this.getLegendaryAttackElixir() + this.getCommonAttackElixir()));
+
+            if (this.verifyCombat()) {
+                System.out.println("---------- FIGHT! ----------\n" +
+                        this.getName() + " use SPECIAL ATTACK " + targetFighter.getName() + "\n" +
+                        "\nOld status: \n" +
+                        this.getName() + "= HP: " + this.getHp() + " | Elixir: " + (this.getElixir() + (this.getLegendaryAttackElixir() + this.getCommonAttackElixir())) + "\n" +
+                        targetFighter.getName() + "= HP: " + targetFighter.getHp() + " | Elixir: " + targetFighter.getElixir());
+
+                targetFighter.setHp(targetFighter.getHp() - (this.getLegendaryAttack() + this.getMediumAttack()));
+
+                System.out.println("\nNew Status: \n" +
+                        this.getName() + "= HP: " + this.getHp() + " | Elixir: " + this.getElixir() +
+                        " (lost " + (this.getLegendaryAttackElixir() + this.getCommonAttackElixir()) + " elixir)\n" +
+                        targetFighter.getName() + "= HP: " + targetFighter.getHp() + " | Elixir: " + targetFighter.getElixir() +
+                        " (lost " + (this.getLegendaryAttack() + this.getMediumAttack()) + " hp)" +
+                        "\n---------- FIGHT ENDED! ----------\n");
+                if (!targetFighter.verifyCombat()) {
+                    System.out.println("\n   ---------- " + this.getName().toUpperCase() + " YOU WIN! (" +
+                            targetFighter.getName() + " HP is over) ----------   \n");
+                    this.loseCombat();
+                }
+
+            } else {
+                System.out.println(this.getName() + " SPECIAL ATTACK " + targetFighter.getName() + "\n" +
+                        "\nOld status: \n" +
+                        this.getName() + "= HP: " + this.getHp() + " | Elixir: " + (this.getElixir() + (this.getLegendaryAttackElixir() + this.getCommonAttackElixir())) + "\n" +
+                        targetFighter.getName() + "= HP: " + targetFighter.getHp() + " | Elixir: " + targetFighter.getElixir() + "\n" +
+                        "\nNew Status: \n" +
+                        this.getName() + "= HP: " + this.getHp() + " | Elixir: " + this.getElixir());
+                System.out.println(this.getName() + " YOU LOSE! (your elixir is over)" +
+                        "\n---------- FIGHT ENDED ----------\n");
+                this.loseCombat();
+            }
+        } else {
+            this.status();
+            targetFighter.status();
+            System.out.println(this.getName() + " and/or " + targetFighter.getName() + " already lose" +
+                    "\n---------- FIGHT ENDED! ----------\n");
+        }
     }
 }
